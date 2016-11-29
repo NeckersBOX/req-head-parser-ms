c3acd6d4596ac70115d9a870689fa95ec93d9c32
Davide Francesco Merico <hds619@gmail.com>
2016-11-29 19:10:33 +0100

    Request Header Parser Microservice

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require ('express');
+let app = express ();
+
+app.get ('*', (req, res) => {
+  res.writeHead (200, { 'Content-Type': 'application/json' });
+
+  res.end (JSON.stringify ({
+    ipaddress: req.connection.remoteAddress,
+    language: req.headers['accept-language'].split (',')[0],
+    software: req.headers['user-agent'].split ('(')[1].split (')')[0]
+  }));
+});
+
+app.listen (process.env.PORT || 8080);
