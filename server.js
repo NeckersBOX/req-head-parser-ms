8855e95f246338d7d2bfedfa320c798df3433953
Davide Francesco Merico <hds619@gmail.com>
2016-11-29 19:18:08 +0100

    add ipware dep

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const express = require ('express');
+const getip = require('ipware')().get_ip;
 let app = express ();
 
 app.get ('*', (req, res) => {
   res.writeHead (200, { 'Content-Type': 'application/json' });
 
   res.end (JSON.stringify ({
-    ipaddress: JSON.stringify (req.connection),
+    ipaddress: getip (req).clientIp,
     language: req.headers['accept-language'].split (',')[0],
     software: req.headers['user-agent'].split ('(')[1].split (')')[0]
   }));
