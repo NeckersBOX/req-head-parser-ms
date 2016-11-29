75bb06520839e1fe1cc2c8e07bcace30b684e494
Davide Francesco Merico <hds619@gmail.com>
2016-11-29 19:13:41 +0100

    Check IP..

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get ('*', (req, res) => {
   res.writeHead (200, { 'Content-Type': 'application/json' });
 
   res.end (JSON.stringify ({
-    ipaddress: req.connection.remoteAddress,
+    ipaddress: JSON.stringify (req.connection),
     language: req.headers['accept-language'].split (',')[0],
     software: req.headers['user-agent'].split ('(')[1].split (')')[0]
   }));
